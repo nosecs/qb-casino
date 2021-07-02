f4efb496602e9012f9b85b1725e2cb94f837c706
GhzGarage <gigahertzgarage@mail.com>
2021-07-01 21:44:12 -0500

    GetObject

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,8 +4,8 @@ game 'gta5'
 description 'QB-Casino'
 version '1.0.0'
 
+shared_script '@qb-core/import.lua'
 client_script 'client/main.lua'
-
 server_script 'server/main.lua'
 
 dependency 'qb-blackjack'
